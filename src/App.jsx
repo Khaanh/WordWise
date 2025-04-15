e35af51fbc110d66b722e8a8a9f361c4370ad861
Khaanh <khanh13nguyen13@gmail.com>
2025-04-16 00:15:55 +0200

    feat: create city list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
+
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -44,7 +47,10 @@ export default function App() {
 						path="cities"
 						element={<CityList cities={cities} isLoading={isLoading} />}
 					/>
-					<Route path="countries" element={<p>Countries</p>} />
+					<Route
+						path="countries"
+						element={<CountryList cities={cities} isLoading={isLoading} />}
+					/>
 					<Route path="form" element={<p>Form</p>} />
 				</Route>
 				<Route path="*" element={<PageNotFound />} />
