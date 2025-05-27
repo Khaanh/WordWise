c2bcb87b9525d747124f6c0363ef0106abc03d4d
Khaanh <khanh13nguyen13@gmail.com>
2025-05-28 00:51:15 +0200

    feat: add pages

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,7 @@
-import PageNav from "../components/PageNav";
-
 export default function PageNotFound() {
-	return (
-		<div>
-			<PageNav />
-			<h1>Page Not Found</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Page not found 😢</h1>
+    </div>
+  );
 }
