7d84a2c37e7b1e35210bb8ae05f11130ff30b1da
Khaanh <khanh13nguyen13@gmail.com>
2025-06-03 21:22:21 +0200

    feat: add city component

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,18 +1,15 @@
-import { useCities } from "../contexts/CitiesContex";
 import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Message from "./Message";
 import Spinner from "./Spinner";
 
-export default function CityList() {
-	const { cities, isLoading } = useCities();
+export default function CityList({ cities, isLoading }) {
 	if (isLoading) return <Spinner />;
 
 	if (!cities.length)
 		return (
-			<Message message="add your first city by clicking on a city on the map" />
+			<Message message="Add your first city by clicking on a city on the map" />
 		);
-
 	return (
 		<ul className={styles.cityList}>
 			{cities.map((city) => (
