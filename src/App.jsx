24971ac1ebbb8a85451ed8efcdb75971a03ac541
Khaanh <khanh13nguyen13@gmail.com>
2025-06-17 23:14:31 +0200

    feat: add useNavigate hook and Button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -41,10 +42,7 @@ export default function App() {
 				<Route path="login" element={<Login />} />
 				<Route path="app" element={<AppLayout />}>
 					{/* Route index - default route */}
-					<Route
-						index
-						element={<CityList cities={cities} isLoading={isLoading} />}
-					/>
+					<Route index element={<Navigate replace to={"cities"} />} />
 					<Route path="cities/:id" element={<City />} />
 					<Route
 						path="cities"
@@ -54,7 +52,7 @@ export default function App() {
 						path="countries"
 						element={<CountryList cities={cities} isLoading={isLoading} />}
 					/>
-					<Route path="form" element={<p>Form</p>} />
+					<Route path="form" element={<Form />} />
 				</Route>
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,11 @@ import styles from "./Button.module.css";
 
 export default function Button({ children, onClick, type }) {
 	return (
-		<button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+		<button
+			className={`${styles.btn} ${styles[type]}`}
+			type={type}
+			onClick={onClick}
+		>
 			{children}
 		</button>
 	);
